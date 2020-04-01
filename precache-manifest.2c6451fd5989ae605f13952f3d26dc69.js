c628e6635c246348f2b83e3c34ab2780e4cce305
Renée Cruz <reneebasscruz@gmail.com>
2020-03-31 22:46:13 -0400

    Updates

---
+++ precache-manifest.2c6451fd5989ae605f13952f3d26dc69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041ac14df0c746060b36ac433607915b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "352a4160cb76aa2039fd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d745a78a75dddae813f",
+    "url": "/static/js/2.294f7882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.294f7882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352a4160cb76aa2039fd",
+    "url": "/static/js/main.5c4b2337.chunk.js"
+  },
+  {
+    "revision": "410e1b418b6bedb8caa7",
+    "url": "/static/js/runtime-main.ec35eec2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
